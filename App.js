742fa375ffaf2eb974c957fecedee99f75d26493
Suvadeep <maityamalendu18@gmail.com>
2021-11-07 21:21:10 +0530

    react native todo app basic layout and style part complete

--- App.js
+++ App.js
@@ -1,21 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
 
-export default function App() {
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  StatusBar,
+  TextInput,
+  FlatList,
+  // ScrollView,
+} from "react-native";
+
+const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTodo = () => {
+    if (!todos) {
+      return;
+    } else {
+      setTodos([...todos, { id: Date.now(), text: todo }]);
+      setTodo("");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.heading}>React Native Todo App</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={(text) => setTodo(text)}
+          value={todo}
+          placeholder="Enter your work here"
+          style={styles.input}
+        />
+        <TouchableOpacity onPress={handleAddTodo}>
+          <Text style={styles.button}>Create</Text>
+        </TouchableOpacity>
+      </View>
+      {/* <ScrollView> */}
+      {/* {todos.map((todo)=>( */}
+      {/* <Text key={todo.id}>{todo.text}</Text> */}
+      {/* ))} */}
+      {/* </ScrollView> */}
+      <View>
+        <FlatList
+          data={todos}
+          renderItem={({ item }) => <Text>{item.text}</Text>}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    backgroundColor: "#F7DAD9",
+  },
+  heading: {
+    marginVertical: 10,
+    fontSize: 30,
+    fontWeight: "700",
+  },
+  input: {
+    flex: 1,
+    backgroundColor: "white",
+    shadowColor: "black",
+    elevation: 10,
+    marginRight: 5,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 50,
+  },
+  button: {
+    padding: 13,
+    backgroundColor: "white",
+    borderRadius: 50,
+    elevation: 10,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    marginHorizontal: 10,
+    alignItems: "center",
   },
 });
